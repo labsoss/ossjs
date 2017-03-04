cc42697283add0752756d9f07f3fbe931efda614
Nikodim Lazarov <nlazarov@gmail.com>
2017-03-04 16:11:57 +0200

    test: add first tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,27 @@
 var expect = require('chai').expect;
 var books = require('./index');
 
+
 describe('books', function() {
-    it('should work', function() {
-        expect(true).to.be.true;
+    var isArrayOfStrings = function(array) {
+        return array.every(function(item) {
+            return typeof(item) === 'string';
+        });
+    }
+
+    describe('all', function() {
+        it('should be an array of strings', function() {
+            expect(books.all).to.satisfy(isArrayOfStrings);
+        });
+
+        it('should contain `Understanding Cryptography`', function() {
+            expect(books.all).to.include('Understanding Cryptography');
+        });
+    });
+
+    describe('random', function() {
+        it('any item', function() {
+            expect(books.all).to.include(books.random());
+        });
     });
 });

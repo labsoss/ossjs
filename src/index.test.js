d9a5157f5d781969ca64c8fd3b8183307b76aa02
Nikodim Lazarov <nlazarov@gmail.com>
2017-03-04 15:33:56 +0200

    chore(testing): Add mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var books = require('./index');
+
+describe('books', function() {
+    it('should work', function() {
+        expect(true).to.be.true;
+    });
+});
